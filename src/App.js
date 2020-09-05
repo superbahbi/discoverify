72b887bbfec34cdf1ac65970b5b2ce96acf691bd
Bobby Lee <superbahbi@users.noreply.github.com>
2020-09-05 10:26:22 -0700

    Updated app js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button, Heading, Container, Section, Columns, Content } from 'react-bulma-components';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <Section>
+      <Container>
+        <Heading className="Title">
+          Playlistify
+        </Heading>
+      </Container>
+    </Section>
+    <Section>
+      <Container>
+        <Columns>
+          <Columns.Column className="Column">
+            <Content>
+              <p>Want to find out how much storage it costs to download your Spotify library as mp3? No? Well find out by pressing the button below!</p>
+              <Button color="black">Sign in with Spotify</Button>
+            </Content>
+          </Columns.Column>
+          <Columns.Column>
+          </Columns.Column>
+        </Columns>
+      </Container>
+    </Section>
+
+    <Section>
+      <Container>
+        <Content className="Footer">
+          <p>
+          Playlistify by <a href="http://bahbi.net">Bahbi</a>. The source code is licensed <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. <a href="https://github.com/superbahbi/Playlistify">Github</a> 
+          </p>
+        </Content>
+      </Container>
+    </Section>
+
+  </div>
   );
 }
-
 export default App;
